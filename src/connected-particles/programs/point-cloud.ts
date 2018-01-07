02fe8152ca3a341455d0ca94018da9070bde226a
kantedal <kantedal@gmail.com>
2018-01-08 00:57:31 +0100

    Random point sizes

--- src/connected-particles/programs/point-cloud.ts
+++ src/connected-particles/programs/point-cloud.ts
@@ -7,13 +7,14 @@ import Shader, { IUniform, IUniforms, UniformTypes } from '../utils/shader'
 // language=GLSL
 const vertexShaderPointsSrc = `#version 300 es
   in vec2 a_textureCoords;
+  in float a_pointSize;
 
   uniform sampler2D pointPositions;
 
   void main() {
     vec3 pos = texture(pointPositions, a_textureCoords).xyz;
     gl_Position = vec4(pos, 1.0);
-    gl_PointSize = 5.0;
+    gl_PointSize = a_pointSize;
   }
 `
 
@@ -45,7 +46,9 @@ export default class PointCloud {
   // Point and line data
   private _pointTextureCoords: Float32Array
   private _pointTextureCoordsBuffer: WebGLBuffer | null
-
+  private _pointSizes: Float32Array
+  private _pointSizesBuffer: WebGLBuffer | null
+  
   constructor() {
     this._pointsShader = new Shader(vertexShaderPointsSrc, fragmentShaderPointsSrc)
     this._pointsShaderUniforms = {
@@ -55,13 +58,23 @@ export default class PointCloud {
     this._shaderProgramPoints = createProgram(gl, this._pointsShader)
 
     const pointTextureCoords: number[] = []
+    const pointSizes: number[] = []
     for (let x = 0; x < 8; x++) {
       for (let y = 0; y < 8; y++) {
         pointTextureCoords.push(x / 8.0), pointTextureCoords.push(y / 8.0)
+        pointSizes.push(Math.random() * 5.0 + 2.0)
       }
     }
     this._pointTextureCoords = new Float32Array(pointTextureCoords)
+    this._pointSizes = new Float32Array(pointSizes)
+
     this._pointTextureCoordsBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._pointTextureCoordsBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, this._pointTextureCoords, gl.STATIC_DRAW)
+
+    this._pointSizesBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._pointSizesBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, this._pointSizes, gl.STATIC_DRAW)
   }
 
   public render(pointPositions: WebGLTexture) {
@@ -70,10 +83,14 @@ export default class PointCloud {
     this._pointsShaderUniforms.pointPositions.value = pointPositions
 
     gl.bindBuffer(gl.ARRAY_BUFFER, this._pointTextureCoordsBuffer)
-    gl.bufferData(gl.ARRAY_BUFFER, this._pointTextureCoords, gl.STATIC_DRAW)
     const textureCoordsPoints = gl.getAttribLocation(this._shaderProgramPoints, 'a_textureCoords')
     gl.vertexAttribPointer(textureCoordsPoints, 2, gl.FLOAT, false, 0, 0)
     gl.enableVertexAttribArray(textureCoordsPoints)
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._pointSizesBuffer)
+    const pointSizesLocation = gl.getAttribLocation(this._shaderProgramPoints, 'a_pointSize')
+    gl.vertexAttribPointer(pointSizesLocation, 1, gl.FLOAT, false, 0, 0)
+    gl.enableVertexAttribArray(pointSizesLocation)
 
     // gl.enable(gl.DEPTH_TEST)
     gl.viewport(0, 0, window.innerWidth, window.innerHeight)
